00c73f56d377876a16ad0c9b3376458fb18b5f17
Alix Leve <alix.leve@gmail.com>
2024-11-16 12:49:33 +0100

    add: README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Palindrome
+
+Programme permettant de vérifier si une date est un palindrome et d'obtenir les prochaines dates palindrome à partir de la date du jour.
+
+## Objectifs à atteindre via cet exercice
+
+- entrainement avec le langage Javascript
+- exploitation de méthodes propres à Javascript comme getDate(), setDate(), getMonth(), getDay(), getFullYear() et de l'objet Date avec le construceur new Date()
+- utilisation de listes pour décomposer la date
+- exécuter des boucles for et des conditions if/else pour gérer l'ordre d'exécution des opérations
+
+## Features
+
+- deux fonctions principales: getNextPalindromes(number) et isPalindrome("dateEnString")
+- on peut obtenir les x prochaines dates palindrome à partir de la date du jour en appelant la fonction getNextPalindromes(x)
+- on peut vérifier si une date est un palindrome en appelant la fonction isPalindrome("dateEnString")
+
+## Tech Stack
+
+- Langage : JavaScript
+
+## Installation
+
+- télécharger le fichier script.js et le lancer avec un éditeur de code.
--- script3.js
+++ script3.js
@@ -1,136 +1,130 @@
 jourMaxParMois = {
-    '01': 31,
-    '02': 29,
-    '03': 31,
-    '04': 30,
-    '05': 31,
-    '06': 30,
-    '07': 31,
-    '08': 31,
-    '09': 30,
-    '10': 31,
-    '11': 30,
-    '12': 31
+	"01": 31,
+	"02": 29,
+	"03": 31,
+	"04": 30,
+	"05": 31,
+	"06": 30,
+	"07": 31,
+	"08": 31,
+	"09": 30,
+	10: 31,
+	11: 30,
+	12: 31,
+};
+
+function verifierFormatDate(date) {
+	const divisionDate = date.split("/");
+	//console.log(`Résultat :  ${divisionDate}`);
+
+	const jour = parseInt(divisionDate[0]);
+	const mois = parseInt(divisionDate[1]);
+	const annee = parseInt(divisionDate[2]);
+
+	//vérifier que mois rentre dans tranche 12 mois
+	if (mois > 12) {
+		//console.log("impossible");
+		return false;
+	}
+
+	//vérifier que jour rentre dans tranche 31 jours
+	if (jour > 31) {
+		//console.log("faux");
+		return false;
+	}
+
+	//vérifier année
+	if (annee < 999 || annee > 9999) {
+		//console.log(date[2]);
+		//console.log("C'est faux !");
+		return false;
+	} else {
+		// console.log("C'est vrai");
+	}
+
+	//vérifier année bisextile
+	if (annee % 4 != 0) {
+		//console.log("Année non-bisextile")
+		jourMaxParMois["02"] < 29;
+	}
+
+	//vérifier jour max pour chaque mois
+	if (jour > jourMaxParMois[divisionDate[1]]) {
+		// console.log("mauvais jour");
+		return false;
+	} else {
+		//console.log("OK");
+	}
+	return divisionDate;
 }
 
+function isPalindrome(dateEnString) {
+	const resultat = verifierFormatDate(dateEnString);
+	console.log("RESULTAT: ", resultat);
 
+	const date = [];
 
-function verifierFormatDate(date){
-const divisionDate = date.split('/');
-//console.log(`Résultat :  ${divisionDate}`);
+	for (let i = 0; i < dateEnString.length; i++) {
+		if (dateEnString[i] == "/") {
+			date.push("");
+		} else {
+			date.push(dateEnString[i]);
+		}
+	}
+	console.log("DATE: ", date);
 
-const jour = parseInt(divisionDate[0]);
-const mois = parseInt(divisionDate[1]);
-const annee = parseInt(divisionDate[2]);
+	const nouvelleDate = date.join("");
+	nouvelleDate.trim();
+	console.log("NOUVELLE DATE: ", nouvelleDate);
 
-//vérifier que mois rentre dans tranche 12 mois
-if(mois > 12){
-    //console.log("impossible");
-    return false;
+	// inverser la date pour future comparaison
+	let dateInversee = "";
+	for (let i = nouvelleDate.length - 1; i >= 0; i--) {
+		dateInversee += nouvelleDate[i];
+	}
+
+	// comparaison paramètre d'entrée et date inversée
+	if (nouvelleDate === dateInversee) {
+		console.log(nouvelleDate, dateInversee);
+		console.log("La date est bien un palindrome");
+		return true;
+	} else {
+		console.log(nouvelleDate, dateInversee);
+		console.log("La date n'est pas un palindrome");
+		return false;
+	}
 }
 
-    //vérifier que jour rentre dans tranche 31 jours
-    if(jour > 31){
-        //console.log("faux");
-        return false; 
-    }
-
-    //vérifier année
-    if (annee < 999 || annee > 9999){
-    //console.log(date[2]);
-    //console.log("C'est faux !");
-    } else {
-   // console.log("C'est vrai");
-    }
-
-    //vérifier année bisextile
-    if (annee % 4 != 0){
-    //console.log("Année non-bisextile")
-    jourMaxParMois['02'] < 29; 
-    }
-
-    //vérifier jour max pour chaque mois
-    if (jour > jourMaxParMois[divisionDate[1]]){
-       // console.log("mauvais jour");
-        return false
-    } else {
-        //console.log("OK");
-    }
-   return divisionDate;
+// methode getDate() retourne le jour du mois de la date indiquée
+// methode setDate() permet de changer le jour du mois
+function getNextDate(date) {
+	date.setDate(date.getDate() + 1);
+	console.log("NEXT DATE: ", date);
 }
 
+function getNextPalindromes(x) {
+	const dateActuelle = new Date();
 
+	while (x > 0) {
+		let jour = dateActuelle.getDate();
+		let mois = dateActuelle.getMonth() + 1;
+		let anneeComplete = dateActuelle.getFullYear();
 
+		if (jour < 10) {
+			jour = "0" + jour;
+		}
 
-function isPalindrome(dateEnString){
-    const resultat = verifierFormatDate(dateEnString);
-    //console.log (resultat);
-
-    const date = [];
-   
-   for (let i = 0; i < dateEnString.length; i++){
-    if(dateEnString[i] == "/"){
-        date.push('');
-    } else {
-        date.push(dateEnString[i]);
-    }
-    //console.log(date);
-   }
-
-  const nouvelleDate = date.join('');
-  nouvelleDate.trim();
-  //console.log(nouvelleDate);
-   
-// inverser la date pour future comparaison
-   let dateInversee = '';
-   for (let i = nouvelleDate.length -1; i >= 0; i --){
-    dateInversee += nouvelleDate[i];
-   } 
-
-// comparaison paramètre d'entrée et date inversée
-   if(nouvelleDate === dateInversee){
-    //console.log(nouvelleDate, dateInversee);
-    //console.log("La date est bien un palindrome");
-    return true;
-   } else {
-    //console.log(nouvelleDate, dateInversee);
-    //console.log("La date n'est pas un palindrome");
-    return false;
-   }
-
-   return nouvelleDate;
-}
-
-
-
-function getNextDate(date){
-    date.setDate(date.getDate() +1);
-    // return date
-}
-
-
-function getNextPalindromes(x){
-    const dateActuelle = new Date();
-    //getNextDate(dateActuelle);
-    while (x > 0){
-        let jour = dateActuelle.getDate();
-        let mois = dateActuelle.getMonth() + 1 ;
-        let anneeComplete = dateActuelle.getFullYear();
-
-        if (jour < 10){
-        jour = '0'+jour
-        }
-
-        if (mois < 10){
-            mois = '0'+mois
-        }
-        if (isPalindrome(`${jour}/${mois}/${anneeComplete}`))
-        {
-            console.log(`${jour}/${mois}/${anneeComplete}`)
-            x --
-        }
-        getNextDate(dateActuelle);
-    }
+		if (mois < 10) {
+			mois = "0" + mois;
+		}
+		// insertion dynamique de variable dans la string date
+		if (isPalindrome(`${jour}/${mois}/${anneeComplete}`)) {
+			console.log(`${jour}/${mois}/${anneeComplete}`);
+			x--;
+		}
+		getNextDate(dateActuelle);
+	}
 }
 
 getNextPalindromes(1);
+// isPalindrome("20/02/2022");
